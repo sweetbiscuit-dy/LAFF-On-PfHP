6c94d5d0aa1df472860c93f9bab855a4f253f18d
dingyu <hsyfzdy@163.com>
2023-05-27 13:41:44 +0800

    impl on Gemm_P_Ger.c and Ger_I_Axpy.c

--- Assignments/Week1/C/Gemm_P_Ger.c
+++ Assignments/Week1/C/Gemm_P_Ger.c
@@ -2,12 +2,13 @@
 #define beta( i,j )  B[ (j)*ldB + i ]   // map beta( i,j )  to array B
 #define gamma( i,j ) C[ (j)*ldC + i ]   // map gamma( i,j ) to array C
 
+// MyGer is rank-1 update
 void MyGer( int, int, double *, int, double *, int, double *, int );
 
 void MyGemm( int m, int n, int k, double *A, int ldA,
 	     double *B, int ldB, double *C, int ldC )
 {
   for ( int p=0; p<k; p++ )
-    MyGer( m, n, &alpha(  ,  ),   , &beta(  ,  ),   , C, ldC );
+    MyGer( m, n, &alpha(0, p), 1, &beta(p, 0), ldB, C, ldC );
 }
   
