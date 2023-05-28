55209bf92c50b4c978c036747c3142adbfd42fb3
dingyu <hsyfzdy@163.com>
2023-05-28 14:03:44 +0800

    complete Gemm_J_dgemv.c

---
+++ Assignments/Week1/C/Gemm_J_dgemv.c
@@ -0,0 +1,20 @@
+#define alpha( i,j ) A[ (j)*ldA + i ]   // map alpha( i,j ) to array A 
+#define beta( i,j )  B[ (j)*ldB + i ]   // map beta( i,j )  to array B
+#define gamma( i,j ) C[ (j)*ldC + i ]   // map gamma( i,j ) to array C
+
+// void MyGemv( int, int, double *, int, double *, int, double *, int );
+void dgemv_(char *, int *, int *, double *, double *, int *, double *, int *, double *, double *, int *);
+
+void MyGemm( int m, int n, int k,
+	     double *A, int ldA,
+	     double *B, int ldB,
+	     double *C, int ldC )
+{
+    int i_one = 1;
+    double d_one = 1.0;
+
+  for ( int j=0; j<n; j++ )
+    // MyGemv(m, k, A, ldA, &beta(0, j), 1, &gamma(0, j), 1);
+    dgemv_("No transpose", &m, &k, &d_one, A, &ldA, &beta(0, j), &i_one, &d_one, &gamma(0, j), &i_one);
+}
+  
