3c4697425234124d33f1425dbb6f11dc26d606ea
dingyu <hsyfzdy@163.com>
2023-05-28 14:20:42 +0800

    complete Gemm_P_dger.c

---
+++ Assignments/Week1/C/Gemm_P_dger.c
@@ -0,0 +1,18 @@
+#define alpha( i,j ) A[ (j)*ldA + i ]   // map alpha( i,j ) to array A 
+#define beta( i,j )  B[ (j)*ldB + i ]   // map beta( i,j )  to array B
+#define gamma( i,j ) C[ (j)*ldC + i ]   // map gamma( i,j ) to array C
+
+// MyGer is rank-1 update
+// void MyGer( int, int, double *, int, double *, int, double *, int );
+void dger_(int *, int *, double *, double *, int *, double *, int *, double *, int *);
+
+void MyGemm( int m, int n, int k, double *A, int ldA,
+	     double *B, int ldB, double *C, int ldC )
+{
+  double d_one = 1.0;
+  int i_one = 1;
+  for ( int p=0; p<k; p++ )
+    // MyGer( m, n, &alpha(0, p), 1, &beta(p, 0), ldB, C, ldC );
+    dger_(&m, &n, &d_one, &alpha(0, p), &i_one, &beta(p, 0), &ldB, C, &ldC);
+}
+  
